8913142a3e55db5cce2a621ca56e638036b3e7bc
HenryFuerez <HenryFuerez@users.noreply.github.com>
2019-01-30 22:03:01 -0500

    Initial commit

---
+++ config/db.php
@@ -0,0 +1,24 @@
+<?php
+/*Datos de conexion a la base de datos*/
+define('DB_HOST', 'localhost');//DB_HOST:  generalmente suele ser "127.0.0.1"
+define('DB_USER', 'root');//Usuario de tu base de datos
+define('DB_PASS', '');//Contraseña del usuario de la base de datos
+define('DB_NAME', 'tienda_prod');//Nombre de la base de datos
+ 
+/*Datos de la empresa*/
+define('NOMBRE_EMPRESA', 'SISTEMA DE VENTAS DE CELULARES Y ACCESORIOS');
+define('DIRECCION_EMPRESA', 'IBARRA, IMBABURA, ECUADOR');
+define('TELEFONO_EMPRESA', '+(503) 0968061259');
+define('EMAIL_EMPRESA', 'proyecto.appinf@utn.edu.ec');
+define('TAX', '12');
+
+/*-------------------------------------------------
+Los datos por defecto del usuario administrador son:
+#usuario: admin
+#contraseña: admin
+Los datos de inicio de sesion podran ser modificados 
+desde el panel de control del sistema
+---------------------------------------------------*/
+
+
+?>
